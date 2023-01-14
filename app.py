a79d1995ebb75bf598d26103e7a2030a58414258
Ben Selleslagh <58362590+bselleslagh@users.noreply.github.com>
2023-01-14 13:40:09 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/whatsapp', methods=['GET'])
+def handle_verification():
+    mode = request.args.get('hub.mode')
+    challenge = request.args.get('hub.challenge')
+    verify_token = request.args.get('hub.verify_token')
+    if mode == 'subscribe' and verify_token == 'vitothecat':
+        return challenge
+    else:
+        return 'Verification token mismatch', 403
